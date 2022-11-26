b512903bd8080836736d7a9327bf20a460f32477
TerraKnuckles <86143143+TerraKnuckles@users.noreply.github.com>
2022-11-26 00:54:19 -0300

    Update main.py

--- Mega Dino/main.py
+++ Mega Dino/main.py
@@ -19,13 +19,15 @@ def main():
 
 if __name__ == '__main__':
     import sys, pygame
-    from screen_size import SCREEN_WIDTH, SCREEN_HEIGHT
 
     pygame.init()
 
     pygame.display.set_caption('Mega Dino')
     pygame.display.set_icon(pygame.image.load('Images/Dino_(to_right)/dino_standing_damaged.png'))
 
+    SCREEN_WIDTH = 1078
+    SCREEN_HEIGHT = 630
+    
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     from groups import *
